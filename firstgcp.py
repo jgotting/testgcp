0c4b394207b9a93c8db2fe69463d065901e174e5
jgotting <johan@gotting.com>
2019-08-24 12:31:50 +0200

    Second commit

--- firstgcp.py
+++ firstgcp.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hey there Johan Gotting och GCP!"
+    return "Hey there Johan Gotting and GCP and App Engine!"
 if __name__ == '__main__':
     app.run(debug=True)
