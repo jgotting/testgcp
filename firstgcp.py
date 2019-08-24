3f04cfa33ea4c1f00a7a2df2eb1011f08f77f463
jgotting <johan@gotting.com>
2019-08-24 13:59:25 +0200

    deleted file

--- firstgcp.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Hey there Johan Gotting and GCP and App Engine!!!"
-if __name__ == '__main__':
-    app.run(debug=True)
